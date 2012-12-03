453418a515ff67c6407b94dd7c8f347b17bfda38
Tom Hogans <tomhsx@gmail.com>
2012-12-03 12:49:42 -0500

    Spellcheck working (part1)

--- spellChecker/part1/dictionary.py
+++ spellChecker/part1/dictionary.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 
-import string
-
 
 class Dictionary(dict):
     """ Implements a dict-based object that whose keys are distinct "words"
@@ -13,7 +11,7 @@ class Dictionary(dict):
         if file_name:
             self.load(file_name)
 
-    def add(self, word):
+    def add_word(self, word):
         """ Adds word with a frequency of 1.  If word already exists, 
         increment that key's value by 1 to indicate it was seen again. """
         word = word.lower()
@@ -32,35 +30,6 @@ class Dictionary(dict):
     def load(self, file_name):
         """ Load words from the specified dictionary at file_name. """
         try:
-            with open(file_name, 'w') as f:
-                while True:
-                    next_word = f.readline()
-                    if not next_word:
-                        break
-                    self.add_word(next_word)
+            [self.add_word(w) for w in open(file_name).read().splitlines()]
         except IOError as e:
             print(e)
-        
-
-
-def parseWords(file_name):
-    """ Reads in the specified text field and yields a word whenever a 
-    sequence of two or more ASCII letters (a-z) are followed by a non-
-    alphabetic character.  It is case insensitive and always yields a
-    lower-case version of the word it is emitting. """
-
-    current_word = ""
-    try:
-        with open(file_name, 'r') as f:
-            while True:
-                next_char = f.read(1)
-                if not next_char:
-                    break
-                if next_char in string.ascii_letters:
-                    current_word += next_char
-                else:
-                    if len(current_word) > 1:
-                        yield current_word
-                    current_word = ""
-    except IOError as e:
-        print(e)
--- spellChecker/part1/spellCheck.py
+++ spellChecker/part1/spellCheck.py
@@ -2,13 +2,97 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import string
 
-import spellchecker.dictionary
+from dictionary import Dictionary
+
+
+ADDITIONAL_VALID_WORDS = ['a', 'i']
+
+
+def getUserResponse(word):
+    """ Prompts the user for action concerning the specified word.
+
+    Returns a tuple of (response, new_word), where response is the action
+    given by the user and new_word is the word entered by the user. """
+
+    resp = raw_input("\"{}\": Replace (R), replace all (P), ignore (I), "
+                     "ignore all (N), or exit (E)?  ".format(word)).lower()
+    if resp == 'e':
+        print("Spellcheck aborted.")
+        sys.exit(0)
+    elif resp == 'r':
+        return ('r', raw_input("Enter replacement word: "))
+    elif resp == 'p':
+        return ('p', raw_input("Enter replacement word: "))
+    elif resp == 'i':
+        return ('i', word)
+    elif resp == 'n':
+        return ('n', word)
+    else:
+        # Ask again
+        return getUserResponse(word)
 
 
 def main():
     files = sys.argv[1:]
-    print(files)
+
+    if not files:
+        print("No input file")
+        return
+
+    if len(files) > 1:
+        print("Cannot spellcheck more than one document at a time")
+
+    file_name = files[0]
+
+    # Set up dictionary based on words.dat
+    d = Dictionary(file_name="words.dat")
+    [d.add_word(w) for w in ADDITIONAL_VALID_WORDS]
+
+    file_in = open(file_name, 'r')
+    file_out = open("{}.out".format(file_name), 'w')
+
+    replacements = {}
+    ignored_words = []
+    current_word = ""
+
+    while True:
+        next_char = file_in.read(1)
+        if not next_char:
+            break
+        if next_char in string.ascii_letters:
+            current_word += next_char
+        else:
+            if len(current_word) > 1:
+                # Check if we should automatically ignore or replace the word
+                if current_word.lower() in ignored_words:
+                    # Take no action
+                    pass
+                elif current_word.lower() in replacements:
+                    # Replace the word with previously specified word
+                    current_word = replacements[current_word]
+                else:
+                    # No automatic action taken, so check if it's OK
+                    if not current_word.lower() in d:
+                        (resp, new_word) = getUserResponse(current_word)
+                        if resp is 'r':
+                            current_word = new_word
+                        elif resp is 'p':
+                            replacements[current_word.lower()] = new_word
+                            current_word = new_word
+                        elif resp is 'i':
+                            # Ignore misspelled word this time
+                            pass
+                        elif resp is 'n':
+                            ignored_words.append(current_word)
+                file_out.write(current_word)
+            else:
+                file_out.write(current_word)
+            current_word = ""
+            file_out.write(next_char)
+
+    print("Spellchecked file written to {}.out.".format(files[0]))
 
 
 if __name__ == "__main__":

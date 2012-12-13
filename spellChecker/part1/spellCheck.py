4f8c6b588b8f780130eb15b6d92fc145aca0b541
Tom Hogans <tomhsx@gmail.com>
2012-12-12 23:58:56 -0500

    Update part1 to project specifications (verify and update method on dictionary)

--- spellChecker/part1/spellCheck.py
+++ spellChecker/part1/spellCheck.py
@@ -2,19 +2,15 @@
 # -*- coding: utf-8 -*-
 
 import sys
-import string
-
 from dictionary import Dictionary
-
-
-ADDITIONAL_VALID_WORDS = ['a', 'i']
 
 
 def getUserResponse(word):
     """ Prompts the user for action concerning the specified word.
 
     Returns a tuple of (response, new_word), where response is the action
-    given by the user and new_word is the word entered by the user. """
+    given by the user and new_word is the word entered by the user (or the
+    original word if 'Ignore' option was chosen. """
 
     resp = raw_input("\"{}\": Replace (R), replace all (P), ignore (I), "
                      "ignore all (N), or exit (E)?  ".format(word)).lower()
@@ -37,13 +33,10 @@ def getUserResponse(word):
 def checkFile(file_name, dictionary_file="words.dat"):
     # Set up dictionary based on words.dat
     d = Dictionary(file_name=dictionary_file)
-    [d.add_word(w) for w in ADDITIONAL_VALID_WORDS]
 
     file_in = open(file_name, 'r')
     file_out = open("{}.out".format(file_name), 'w')
 
-    replacements = {}
-    ignored_words = []
     current_word = ""
 
     while True:
@@ -53,31 +46,16 @@ def checkFile(file_name, dictionary_file="words.dat"):
         if not next_char:
             break
 
-        if next_char in string.ascii_letters:
+        if next_char in d.ALLOWED_LETTERS:
             current_word += next_char
         else:
-            if len(current_word) > 1:
-                # Check if we should automatically ignore or replace the word
-                if current_word.lower() in ignored_words:
-                    pass
-                elif current_word.lower() in replacements:
-                    # Replace the word with previously specified word
-                    current_word = replacements[current_word.lower()]
-                else:
-
-                    # No automatic action taken, so check if it's OK
-                    if not current_word.lower() in d:
-                        (resp, new_word) = getUserResponse(current_word)
-                        if resp is 'r':  # Replace word one time
-                            current_word = new_word
-                        elif resp is 'p':  # Always replace this word
-                            replacements[current_word.lower()] = new_word
-                            current_word = new_word
-                        elif resp is 'n':  # Always ignore this word
-                            ignored_words.append(current_word)
-                file_out.write(current_word)
-            else:
-                file_out.write(current_word)
+            # Verify the current_word with the dictionary
+            resp, current_word = d.verify(current_word)
+            if not resp:  # Word was not found in dictionary
+                resp, new_word = getUserResponse(current_word)
+                d.update(resp, current_word, new_word)
+                current_word = new_word
+            file_out.write(current_word)
             current_word = ""
             file_out.write(next_char)
 

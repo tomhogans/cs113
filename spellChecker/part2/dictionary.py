1f55598db1d47e6148dcd4676a7b8447463cdcc5
Tom Hogans <tomhsx@gmail.com>
2012-12-17 23:01:55 -0500

    Implement hash tables in dictionary class

--- spellChecker/part2/dictionary.py
+++ spellChecker/part2/dictionary.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from collections import defaultdict
 import string
 
 
@@ -13,20 +14,24 @@ def makehash(word, m):
     return (26 * makehash(word[1:], m) + ord(word[0])) % m
 
 
-class Dictionary:
+class Dictionary(dict):
     """ Implements a dict-based object that whose keys are distinct "words"
     (where words are any alphabetic sequence of length two or greater) and
-    whose values represent the frequency with which the words appear. """
+    whose values represent the frequency with which the words appear. When
+    words are added with add_word the word and its frequency count is stored
+    as a key: value pair in self.  When a words.dat file is loaded, the words
+    are stored in two different hash tables.  See save() and load(). """
 
     ALLOWED_LETTERS = string.ascii_letters + "'"
     WHITESPACE = string.whitespace
     ADDITIONAL_VALID_WORDS = ['a', 'i']
-    CACHE_SIZE = 5
+    HASH_LOAD_FACTOR = 5000
+    CACHE_SIZE = 500
 
     def __init__(self, file_name=None):
         """ Constructor that accepts an optional file_name to load words. """
-        self.word_list = {}  # Dict of {word: frequency}
-        self.word_cache = {}  # Dict of {word: frequency} for most freq words
+        self.word_list = defaultdict(list)  # Hash table for words
+        self.word_cache = defaultdict(list)  # Hash table for most frequent words
         self.replacement_words = {}
         self.ignored_words = []
         [self.add_word(w) for w in self.ADDITIONAL_VALID_WORDS]
@@ -68,25 +73,43 @@ class Dictionary:
     def add_word(self, word):
         """ Adds word with a frequency of 1.  If word already exists, 
         increment that key's value by 1 to indicate it was seen again. """
+        if len(word) < 2:
+            return
         word = word.lower()
-        if word in self.word_list:
-            self.word_list[word] += 1
+        if word in self:
+            self[word] += 1
         else:
-            self.word_list[word] = 1
+            self[word] = 1
 
     def save(self, file_name):
         """ Save the words to the specified file_name. """
         try:
-            # TODO: Write words in self.cache_list first, then self.word_list
-            open(file_name, 'w').write("\n".join(self.word_list.keys()))
+            sorted_list = sorted(self, key=self.get, reverse=True)
+            with open(file_name, 'w') as file_out:
+                [file_out.write("{}\n".format(w)) 
+                        for w in sorted_list[:self.CACHE_SIZE]]
+                [file_out.write("{}\n".format(w)) 
+                        for w in sorted_list[self.CACHE_SIZE:]]
         except IOError as e:
             print(e)
 
     def load(self, file_name):
         """ Load words from the specified dictionary at file_name. """
         try:
-            # TODO: Load first self.CACHE_SIZE words into self.cache_list, then
-            # load remaining words into 
-            [self.add_word(w) for w in open(file_name).read().splitlines()]
+            with open(file_name, 'r') as file_in:
+                for i in range(self.CACHE_SIZE):
+                    word = file_in.readline().rstrip('\n')
+                    if not word:
+                        break
+                    w_hash = makehash(word, self.HASH_LOAD_FACTOR)
+                    self.word_cache[w_hash].append(word)
+                while True:
+                    word = file_in.readline().rstrip('\n')
+                    if not word:
+                        break
+                    w_hash = makehash(word, self.HASH_LOAD_FACTOR)
+                    self.word_list[w_hash].append(word)
+            print("Cache", self.word_cache)
+            print("List", self.word_list)
         except IOError as e:
             print(e)
